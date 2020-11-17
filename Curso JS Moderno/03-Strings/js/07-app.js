3c75c90ddf3d8cdf44b99cd2f8d50c5de037cd8f
Carlos Astorga <carlos.astorgax@gmail.com>
2020-11-17 19:56:52 +0100

    toUpperCase toLowerCase toString

---
+++ Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,15 @@
+const producto = "Monitor 20 Pulgadas ";
+
+/* ---------------------------------MÉTODO toUpperCase (Convierte todo a mayúscula)------------------------------------*/
+
+console.log(producto.toUpperCase());
+
+/* ---------------------------------MÉTODO toLowerCase (Convierte todo a mayúscula)------------------------------------*/
+
+console.log(producto.toLowerCase());
+
+/* ---------------------------------MÉTODO toString (Convierte a String)------------------------------------*/
+
+const precio = 300;
+console.log(precio); //number
+console.log(precio.toString()); //string
